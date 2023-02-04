524682a3af676ea55de78e8ac721b86c64cb6e5f
lynn baydoun <lkb07@mail.aub.edu>
2023-02-04 11:36:28 +0200

    first commit

---
+++ Assets/Game/script/Character.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private CharacterController _cc;
+
+    public float MoveSpeed = 5f;
+
+    private Vector3 _movementVelocity;
+
+    //access to the PlayerInput script
+    private PlayerInput _playerInput;
+
+    private float _verticalVelocity;
+
+    private float Gravity = -9.8f;
+
+    //getting and storing references to the character controller
+    // an Awake function is a built in unity function (its called before the start function)
+    private void Awake()
+    {
+        _cc = GetComponent<CharacterController>();
+        _playerInput = GetComponent<PlayerInput>();
+    }
+    //changing the velocity based on the player's input
+    private void CalculatePlayerMovement()
+    {
+        _movementVelocity.Set(_playerInput.HorizontalInput, 0f, _playerInput.VerticalInput);
+        _movementVelocity.Normalize(); //otherwise the player will move faster when its moving diagonally(this is a built in fct in vector3)
+        _movementVelocity = Quaternion.Euler(0, -45f, 0) * _movementVelocity;//to rotate the movement velocity to match the camera view angle
+        _movementVelocity *= MoveSpeed * Time.deltaTime;//to make the movements smooth across frames (Time.deltaTime is the time passed since last frame)
+
+        if (_movementVelocity != Vector3.zero)
+        { //to stop the character from snapping back to the default position
+            transform.rotation = Quaternion.LookRotation(_movementVelocity); //to make the player rotate
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CalculatePlayerMovement();
+
+        //moving the player downwards when its not on the ground
+        if (_cc.isGrounded == false)
+        {
+            _verticalVelocity = Gravity;
+        }
+        //the else statement here is used to keep the character grounded during the next frame because if we do nothing then the character will automatically be floating in the next frame
+        else
+        {
+            _verticalVelocity = Gravity * 0.3f;
+        }
+        _movementVelocity += _verticalVelocity * Vector3.up * Time.deltaTime;
+        _cc.Move(_movementVelocity);
+    }
+}
---
+++ Assets/Game/script/PlayerInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float HorizontalInput;
+    public float VerticalInput;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
+        VerticalInput = Input.GetAxisRaw("Vertical");
+
+    }
+    private void qonDisable()
+    {
+        HorizontalInput = 0;
+        VerticalInput = 0;
+    }
+}
